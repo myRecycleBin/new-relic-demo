cdd12fe9d5d0126df1d5f1777391f2a6374b9e15
rajasun <rajasun34@gmail.com>
2015-08-14 18:57:45 +0530

    Updated to add a 5 min Aggregated Metric

--- src/main/java/com/emc/demo/aspects/InBoundServiceMetricsAspect.java
+++ src/main/java/com/emc/demo/aspects/InBoundServiceMetricsAspect.java
@@ -10,7 +10,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.util.StopWatch;
 
-import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Counter;
+import com.codahale.metrics.Gauge;
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricFilter;
@@ -21,37 +22,52 @@ import com.codahale.metrics.graphite.GraphiteReporter;
 @Aspect
 public class InBoundServiceMetricsAspect {
   MetricRegistry metricRegistry = new MetricRegistry();
-  
   Graphite graphite = new Graphite(new InetSocketAddress("localhost", 2003));
-  GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry)
-                                                    .convertRatesTo(TimeUnit.MINUTES)
-                                                    .convertDurationsTo(TimeUnit.SECONDS)
-                                                    .filter(MetricFilter.ALL)
-                                                    .build(graphite);
+  GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.MINUTES)
+      .convertDurationsTo(TimeUnit.SECONDS).filter(MetricFilter.ALL).build(graphite);
+
+  MetricRegistry aggRegistry = new MetricRegistry();
+  GraphiteReporter aggReporter = GraphiteReporter.forRegistry(aggRegistry).convertRatesTo(TimeUnit.MINUTES)
+      .convertDurationsTo(TimeUnit.SECONDS).filter(MetricFilter.ALL).build(graphite);
+
   /**
-  ConsoleReporter reporter = ConsoleReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.MINUTES)
-      .convertDurationsTo(TimeUnit.SECONDS).build();
-  **/
+   * ConsoleReporter reporter =
+   * ConsoleReporter.forRegistry(metricRegistry).convertRatesTo(TimeUnit.MINUTES)
+   * .convertDurationsTo(TimeUnit.SECONDS).build();
+   **/
 
   private final Meter requests = metricRegistry.meter("ff.inbound.missedcallcount");
   private final Meter initiateIVRContent = metricRegistry.meter("ff.inbound.missedcalls.contentcount");
   private final Meter reinitiateIVRReg = metricRegistry.meter("ff.inbound.missedcalls.registration.retry");
   private final Meter initiateIVRReg = metricRegistry.meter("ff.inbound.missedcalls.registration.new");
   private final Histogram ivrRegDuration = metricRegistry.histogram("ff.outbound.ivrreg.completed.duration");
+  private final Counter missedCallCounter = metricRegistry.counter("ff.inbound.missedcall.counter");
+  private final Gauge missedCallSum = aggRegistry.register(aggRegistry.name("ff.inbound.missedcall.total"),
+      new Gauge<Integer>() {
+        @Override
+        public Integer getValue() {
+          Counter c = metricRegistry.counter("ff.inbound.missedcall.counter");
+          long count = c.getCount();
+          c.dec(count);
+          return (int) count;
+        }
+      });
 
   public InBoundServiceMetricsAspect() {
     reporter.start(30, TimeUnit.SECONDS);
+    aggReporter.start(5, TimeUnit.MINUTES);
   }
 
   @After("execution(* com.emc.demo.InBoundService.handleInboundMC(..))")
   public void afterMCHandling(JoinPoint joinPoint) {
     System.out.println("---->");
     requests.mark();
+    missedCallCounter.inc();
   }
 
   @Around("execution(private * com.emc.demo.InBoundService.initiateIVRReg())")
   public Object aroundInitingIVRRegCall(ProceedingJoinPoint pjp) throws Throwable {
-    System.out.println("///////>");    
+    System.out.println("///////>");
     StopWatch stp = new StopWatch();
     stp.start();
     Object retVal = pjp.proceed();
